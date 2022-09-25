0e414de250165cb6bdf92505a8f0d8042278be6d
torontopubliclibra <dana.r.teagle@gmail.com>
2022-09-25 11:08:17 -0400

    added slide-out animation to nav and commented hamburger function

--- js/scripts.js
+++ js/scripts.js
@@ -8,13 +8,38 @@
 
   // hamburger menu function (hide/show)
   function hamburger() {
+
+    // if display = false
     if (!nav.style.display) {
+
+      // add slide-in animation
       nav.style.animation = `slide-in linear 0.3s`;
+
+      // make display = block
       nav.style.display = `block`;
+
+    // if display = `block`
     } else {
-      nav.style.display = ``;
+
+      // add slide-out animation
+      nav.style.animation = `slide-out linear 0.3s`;
+
+      // wait until slide-out is complete
+      setTimeout(function() {
+
+        // empty display value
+        nav.style.display = ``;
+      }, 300);
     };
   };
 
-  // hamburger button event listener
+  // run hamburger function on click of hamburger button
     hamburgerBtn.addEventListener(`click`, hamburger);
+
+
+// scroll to top button function
+
+  // scroll function
+  function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      }
